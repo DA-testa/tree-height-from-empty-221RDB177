e8508c5e133cbdd73169a05815051c4482273a30
221RDB177 <125874315+221RDB177@users.noreply.github.com>
2023-03-05 23:26:03 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -3,30 +3,41 @@
 import sys
 import threading
 
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.children = []
+        
 
-def compute_height(n, parents):
-    max_height = 0
-    return max_height
+def compute_tree_height(node):
+    if not node.children:
+        return 1
+    child_heights = [compute_tree_height(child) for child in node.children]
+    return max(child_heights) + 1
 
 
 def main():
-    input_format = input().strip()
+    # Read input
+    n = int(input())
+    parents = list(map(int, input().split()))
+
+    # Create tree nodes
+    nodes = [TreeNode(i) for i in range(n)]
+    root = None
+
+    # Build tree
+    for i, parent in enumerate(parents):
+        if parent == -1:
+            root = nodes[i]
+        else:
+            nodes[parent].children.append(nodes[i])
+            
+            tree_height = compute_tree_height(root)
+
+    # Print result
+    print(tree_height)
     
     
-    if input_format == "I":
-        
-        n = int(input().strip())
-        parents = list(map(int, input().strip().split()))
-    else:
-        
-        file_name = input().strip()
-        while "a" in file_name:
-            file_name = input().strip()
-        with open(f"./data/{file_name}", "r") as f:
-            n = int(f.readline().strip())
-            parents = list(map(int, f.readline().strip().split()))
- 
-    print(compute_height(n, parents))
 sys.setrecursionlimit(10**7)  
 threading.stack_size(2**27)   
 threading.Thread(target=main).start()
