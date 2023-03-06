ed9f7ac33271be45862636f2322a8a5d4ffd19b5
221RDB177 <125874315+221RDB177@users.noreply.github.com>
2023-03-06 09:03:00 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,40 +1,57 @@
-# python3
-
 import sys
 import threading
+import numpy
+
 
 def compute_height(n, parents):
-   
-    children = {}
-    for node, parent in enumerate(parents):
+    children = {i: [] for i in range(n)}
+    root = [] 
+
+    for i, parent in enumerate(parents):
         if parent == -1:
-            root = node
+            root.append(i)
+        else: 
+            children[parent].append(i)
+ 
+    def fDepth(node, fd):
+        if not children[node]:
+            return fd 
         else:
-            if parent in children:
-                children[parent].append(node)
-            else:
-                children[parent] = [node]
-
-    
-    stack = [(root, 0)]
-    max_height = 0
-    while stack:
-        node, height = stack.pop()
-        max_height = max(max_height, height)
-        if node in children:
+            mDepth = 0 
             for child in children[node]:
-                stack.append((child, height + 1))
+                cDepth = fDepth(child, fd+1) 
+                mDepth = max(mDepth, cDepth)
+            return mDepth 
+ 
+    mHight =  0 
+    for r in root:
+        tHeight = fDepth(r, 0)
+        mHight = max(mHight, tHeight) 
 
-    return max_height
+    return mHight + 1
 
-if __name__ == '__main__':
-  
-    n = int(input())
-    parents = list(map(int, input().split()))
-
-   
-    print(compute_height(n, parents))
+def main():
+    answer = input("Enter F (for file) or I (for input): ")
+    if "I" in answer:
+        n = int(input())
+        parents = list(map(int,input().split()))
+    elif "F" in answer:
+        fileName = input("Enter file name: ")
+        file = "./test/" + fileName
+        if "a" not in fileName:
+            try:
+                with open(file) as testFile:
+                    n = int(testFile.readline())
+                    parents = list(map(int, testFile.readline().split()))
+            except Exception as error:
+                print("Error:", str(error))
+                return
+        else:
+            print("Error. Try again")
+            return
     
-sys.setrecursionlimit(10**7)  
-threading.stack_size(2**27)   
+    print(compute_height(n, parents))
+
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
 threading.Thread(target=main).start()
